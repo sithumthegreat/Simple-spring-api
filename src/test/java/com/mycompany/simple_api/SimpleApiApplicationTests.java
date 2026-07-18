5bd651505ffa6633c8e8534d42a56e702de3c6f6
sithumthegreat <sithumkandage6@gmail.com>
2026-07-18 23:14:37 +0530

    Initial commit: Simple Spring Boot project

---
+++ src/test/java/com/mycompany/simple_api/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.simple_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
